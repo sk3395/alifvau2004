348168b5d67b857faeea5cd72249d7ff8ca4dc35
Alifvau <96680774+Alifvau@users.noreply.github.com>
2022-01-09 07:42:45 +0600

    Update alifvaufdlist.py

--- alifvaufdlist.py
+++ alifvaufdlist.py
@@ -127,7 +127,6 @@ def menu():
   print(" [4] see crack result")
   print(" [5] check the crack result option")
   print(" [6] user-agent settings")
-  print("[ ALIF VAU ]")
   print(" [\033[0;91m0\033[0;97m] exit (remove token)")
   emon = raw_input("\n [?] choose : ")
   if emon == "":
